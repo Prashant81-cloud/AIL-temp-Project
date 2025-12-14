e1d3c41d9bb393581f10aaebc3d2e244421a8011
Prashant81-cloud <prashant81046@gmail.com>
2025-12-15 00:03:44 +0530

    POST err resolved

--- frontend/src/Pages/Career.jsx
+++ frontend/src/Pages/Career.jsx
@@ -22,7 +22,7 @@ function Career() {
 const handleSubmit = async (e) => {
   e.preventDefault();
 
-  const res = await fetch("/api/send-email", {
+  const res = await fetch("/api/send-email-career", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(formData),
